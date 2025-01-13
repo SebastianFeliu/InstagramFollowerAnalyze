72a10217441e2892a65ddbfb486e0a230006f889
Sebastián Feliú Feliú <sebastianfeliufeliu@MacBook-Pro-de-Sebastian.local>
2025-01-12 21:28:01 -0300

    add env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseHref: '/'  // Ruta base para producción
+};
--- src/service/version.service.ts
+++ src/service/version.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  constructor(private http: HttpClient) {}
+  versionData: any;
+  versionUrl: string;
+  constructor(private http: HttpClient) { // Construir la URL dependiendo del entorno
+    this.versionUrl = environment.baseHref + 'version.json';
+    this.getVersion();
+  }
 
   getVersion(): Observable<{ version: string }> {
-    return this.http.get<{ version: string }>('/version.json');
+    return this.http.get<{ version: string }>(this.versionUrl);
   }
 }
